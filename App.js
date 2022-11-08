0cb4c0a94b71b9979274ad1f79f19bb08b73f4df
abhijeetpalanki <ajpalanki@gmail.com>
2022-11-07 19:35:57 -0600

    logic implementation

--- App.js
+++ App.js
@@ -1,15 +1,87 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { colors } from "./src/constants";
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from "react-native";
+import { colors, CLEAR, ENTER } from "./src/constants";
 import Keyboard from "./src/components/Keyboard";
 
+const NUMBER_OF_TRIES = 6;
+
+const copyArray = (arr) => {
+  return [...arr.map((rows) => [...rows])];
+};
+
 export default function App() {
+  const word = "hello";
+  const letters = word.split("");
+
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(""))
+  );
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  const onKeyPressed = (key) => {
+    const updatedRows = copyArray(rows);
+
+    if (key === CLEAR) {
+      const prevCol = curCol - 1;
+      if (prevCol >= 0) {
+        updatedRows[curRow][prevCol] = "";
+        setRows(updatedRows);
+        setCurCol(prevCol);
+      }
+      return;
+    }
+
+    if (key === ENTER) {
+      if (curCol === rows[0].length) {
+        setCurRow(curRow + 1);
+        setCurCol(0);
+      }
+
+      return;
+    }
+
+    if (curCol < rows[0].length) {
+      updatedRows[curRow][curCol] = key;
+      setRows(updatedRows);
+      setCurCol(curCol + 1);
+    }
+  };
+
+  const isCellActive = (row, col) => {
+    return row === curRow && col === curCol;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
 
       <Text style={styles.title}>WORDLE</Text>
-      <Keyboard />
+
+      <ScrollView style={styles.map}>
+        {rows.map((row, i) => (
+          <View style={styles.row} key={`row-${i}`}>
+            {row.map((cell, j) => (
+              <View
+                style={[
+                  styles.cell,
+                  {
+                    borderColor: isCellActive(i, j)
+                      ? colors.lightgrey
+                      : colors.darkgrey,
+                  },
+                ]}
+                key={`cell-${i}-${j}`}
+              >
+                <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+
+      <Keyboard onKeyPressed={onKeyPressed} />
     </SafeAreaView>
   );
 }
@@ -25,5 +97,32 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: "bold",
     letterSpacing: 7,
+    borderBottomColor: colors.lightgrey,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  map: {
+    alignSelf: "stretch",
+    height: 100,
+    marginVertical: 20,
+  },
+  row: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  cell: {
+    borderWidth: 3,
+    borderColor: colors.darkgrey,
+    flex: 1,
+    maxWidth: 70,
+    aspectRatio: 1,
+    margin: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cellText: {
+    color: colors.lightgrey,
+    fontWeight: "bold",
+    fontSize: 28,
   },
 });
